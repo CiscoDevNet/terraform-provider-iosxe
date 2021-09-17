ae02bd0a029fa2f287916f0ad63170cd89c1305e
cmistry-crest <chinmay.mistry@crestdatasys.com>
2021-09-17 19:53:15 +0530

    Edit tf files

--- examples/resources/iosxe_rest/resource.tf
+++ examples/resources/iosxe_rest/resource.tf
@@ -1,10 +1,3 @@
-provider "iosxe" {
-  host = "https://localhost:8443"
-  insecure = true
-  device_password = "Cisco123"
-  device_username = "admin"
-}
-
 resource "iosxe_rest" "rest1" {
   method = "GET"
   path = "/data/Cisco-IOS-XE-native:native"
