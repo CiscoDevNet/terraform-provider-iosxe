259886af220dd4e6bb4dbb98d95b22ec8c8c506b
Akash Gupta <akash.gupta@CDSYS.LOCAL>
2021-09-30 15:18:00 +0530

    Added example and JSON reference for POE

---
+++ examples/examples_tf/poe.tf
@@ -0,0 +1,55 @@
+
+# Adding POE configuration on the interface Gi1/0/12
+resource "iosxe_rest" "poe_example_post" {
+  method = "POST"
+  path = "/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet=1%2f0%2f12/Cisco-IOS-XE-power:power/inline"
+  payload = jsonencode(
+    {
+      "static-choice": {
+        "max": 5000
+      }
+    }
+  )
+}
+
+# Updating the available POE information on the interface Gi1/0/12
+resource "iosxe_rest" "poe_example_put" {
+  method = "PUT"
+  path = "/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet=1%2f0%2f12/Cisco-IOS-XE-power:power/inline"
+  payload = jsonencode(
+    {
+      "Cisco-IOS-XE-power:inline": {
+        "auto-choice": {
+          "max": 4500
+        }
+      }
+    }
+  )
+}
+
+# Adding/Updating the POE configuration on the interface Gi1/0/12
+resource "iosxe_rest" "poe_example_patch" {
+  method = "PATCH"
+  path = "/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet=1%2f0%2f12/Cisco-IOS-XE-power:power/inline"
+  payload = jsonencode(
+    {
+      "Cisco-IOS-XE-power:inline": {
+        "auto-choice": {
+          "max": 5000
+        }
+      }
+    }
+  )
+}
+
+# Fetch the available POE information for interface Gi1/0/12
+resource "iosxe_rest" "poe_example_get" {
+  method = "GET"
+  path = "/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet=1%2f0%2f12/Cisco-IOS-XE-power:power"
+}
+
+# Remove POE capabilities from the interface Gi1/0/12
+resource "iosxe_rest" "poe_example_delete" {
+  method = "DELETE"
+  path = "/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet=1%2f0%2f12/Cisco-IOS-XE-power:power/inline"
+}
