9b6d9a9b28afce46063c9358189060a8ba60b71b
Story DeWeese <sdeweese@alumni.scu.edu>
2022-03-02 15:47:54 -0800

    Update simplified_acl_and_vlan.tf

--- examples/tutorials/acl_and_vlan/simplified_acl_and_vlan.tf
+++ examples/tutorials/acl_and_vlan/simplified_acl_and_vlan.tf
@@ -1,20 +1,4 @@
 # Adding VLAN & extended ACL
-terraform {
-  required_providers {
-    iosxe = {
-      source = "local.plugin/ciscodevnet/iosxe"
-#      version = ">= 0.1"
-    }
-  }
-}
-
-provider "iosxe" {
-  host = "https://10.1.1.5"
-  insecure = true
-  device_username = "admin"
-  device_password = "Cisco123"
-}
-
 resource "iosxe_rest" "vlan_example_put" {
   method = "PUT"
   path = "/data/Cisco-IOS-XE-native:native/vlan/vlan-list=600"
