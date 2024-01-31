15aae7d20e07ae4dc5469a913ad8687d32e633b5
Shankar <swarudka@cisco.com>
2024-01-31 15:06:11 +0530

    US-DIGITIZE-1491

--- examples/resources/iosxe_bgp_ipv4_unicast_vrf_neighbor/resource.tf
+++ examples/resources/iosxe_bgp_ipv4_unicast_vrf_neighbor/resource.tf
@@ -35,4 +35,5 @@ resource "iosxe_bgp_ipv4_unicast_vrf_neighbor" "example" {
   ha_mode_graceful_restart = true
   next_hop_self            = true
   next_hop_self_all        = true
+  advertisement_interval   = 300
 }
