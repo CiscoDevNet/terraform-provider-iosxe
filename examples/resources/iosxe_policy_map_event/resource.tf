6bffb1b0f2f71246efa0c93b437206c3f0f73c45
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2024-05-07 17:35:24 +0200

    policy_map_event: add more actions
    Closes #135

--- examples/resources/iosxe_policy_map_event/resource.tf
+++ examples/resources/iosxe_policy_map_event/resource.tf
@@ -9,11 +9,32 @@ resource "iosxe_policy_map_event" "example" {
       execution_type = "do-until-failure"
       action_numbers = [
         {
-          number                        = 10
-          authenticate_using_method     = "dot1x"
-          authenticate_using_retries    = 2
-          authenticate_using_retry_time = 0
-          authenticate_using_priority   = 10
+          number                                            = 10
+          activate_service_template_config_service_template = "DEFAULT_LINK_POLICY"
+          activate_service_template_config_aaa_list         = "methodlist1"
+          activate_service_template_config_precedence       = 1
+          activate_interface_template                       = "templ1"
+          activate_policy_type_control_subscriber           = "subscriber1"
+          authenticate_using_method                         = "dot1x"
+          authenticate_using_retries                        = 2
+          authenticate_using_retry_time                     = 0
+          authenticate_using_priority                       = 10
+          authenticate_using_aaa_authc_list                 = "listname1"
+          authenticate_using_aaa_authz_list                 = "listname2"
+          authenticate_using_both                           = true
+          replace                                           = true
+          restrict                                          = true
+          clear_session                                     = true
+          clear_authenticated_data_hosts_on_port            = true
+          protect                                           = true
+          err_disable                                       = true
+          resume_reauthentication                           = true
+          authentication_restart                            = 2
+          set_domain                                        = "data"
+          unauthorize                                       = true
+          notify                                            = true
+          set_timer_name                                    = "timer1"
+          set_timer_value                                   = 3600
         }
       ]
     }
--- internal/provider/data_source_iosxe_policy_map_event.go
+++ internal/provider/data_source_iosxe_policy_map_event.go
@@ -117,6 +117,38 @@ func (d *PolicyMapEventDataSource) Schema(ctx context.Context, req datasource.Sc
 										MarkdownDescription: "activate service template",
 										Computed:            true,
 									},
+									"activate_service_template_config_aaa_list": schema.StringAttribute{
+										MarkdownDescription: "Named Method List",
+										Computed:            true,
+									},
+									"activate_service_template_config_precedence": schema.Int64Attribute{
+										MarkdownDescription: "Template precedence",
+										Computed:            true,
+									},
+									"activate_service_template_config_replace_all": schema.BoolAttribute{
+										MarkdownDescription: "Replace all existing authorization data and services",
+										Computed:            true,
+									},
+									"activate_interface_template": schema.StringAttribute{
+										MarkdownDescription: "activate interface template",
+										Computed:            true,
+									},
+									"activate_policy_type_control_subscriber": schema.StringAttribute{
+										MarkdownDescription: "policy type control subscriber",
+										Computed:            true,
+									},
+									"deactivate_interface_template": schema.StringAttribute{
+										MarkdownDescription: "activate interface template",
+										Computed:            true,
+									},
+									"deactivate_service_template": schema.StringAttribute{
+										MarkdownDescription: "activate service template",
+										Computed:            true,
+									},
+									"deactivate_policy_type_control_subscriber": schema.StringAttribute{
+										MarkdownDescription: "policy type control subscriber",
+										Computed:            true,
+									},
 									"authenticate_using_method": schema.StringAttribute{
 										MarkdownDescription: "",
 										Computed:            true,
@@ -131,6 +163,78 @@ func (d *PolicyMapEventDataSource) Schema(ctx context.Context, req datasource.Sc
 									},
 									"authenticate_using_priority": schema.Int64Attribute{
 										MarkdownDescription: "Method priority",
+										Computed:            true,
+									},
+									"authenticate_using_aaa_authc_list": schema.StringAttribute{
+										MarkdownDescription: "Specify authentication method list",
+										Computed:            true,
+									},
+									"authenticate_using_aaa_authz_list": schema.StringAttribute{
+										MarkdownDescription: "Specify authorization method list",
+										Computed:            true,
+									},
+									"authenticate_using_both": schema.BoolAttribute{
+										MarkdownDescription: "Enabling Dot1x Authenticator & Supplicant",
+										Computed:            true,
+									},
+									"authenticate_using_parameter_map": schema.StringAttribute{
+										MarkdownDescription: "Specify parameter map name",
+										Computed:            true,
+									},
+									"replace": schema.BoolAttribute{
+										MarkdownDescription: "clear existing session and create session for violating host",
+										Computed:            true,
+									},
+									"restrict": schema.BoolAttribute{
+										MarkdownDescription: "drop violating packets and generate a syslog",
+										Computed:            true,
+									},
+									"clear_session": schema.BoolAttribute{
+										MarkdownDescription: "clears an active session",
+										Computed:            true,
+									},
+									"clear_authenticated_data_hosts_on_port": schema.BoolAttribute{
+										MarkdownDescription: "clears authenticated data hosts on the port",
+										Computed:            true,
+									},
+									"protect": schema.BoolAttribute{
+										MarkdownDescription: "silently drop violating packets",
+										Computed:            true,
+									},
+									"err_disable": schema.BoolAttribute{
+										MarkdownDescription: "temporarily disable port",
+										Computed:            true,
+									},
+									"resume_reauthentication": schema.BoolAttribute{
+										MarkdownDescription: "resume reauthentication",
+										Computed:            true,
+									},
+									"authentication_restart": schema.Int64Attribute{
+										MarkdownDescription: "restarts the auth sequence after the specified number of sec",
+										Computed:            true,
+									},
+									"set_domain": schema.StringAttribute{
+										MarkdownDescription: "set domain",
+										Computed:            true,
+									},
+									"unauthorize": schema.BoolAttribute{
+										MarkdownDescription: "unauthorize session",
+										Computed:            true,
+									},
+									"notify": schema.BoolAttribute{
+										MarkdownDescription: "notifies the session attributes",
+										Computed:            true,
+									},
+									"set_timer_name": schema.StringAttribute{
+										MarkdownDescription: "timer name",
+										Computed:            true,
+									},
+									"set_timer_value": schema.Int64Attribute{
+										MarkdownDescription: "Enter a value between 1 and 65535",
+										Computed:            true,
+									},
+									"map_attribute_to_service_table": schema.StringAttribute{
+										MarkdownDescription: "map identity-update attribute to a auto-conf templates",
 										Computed:            true,
 									},
 								},
