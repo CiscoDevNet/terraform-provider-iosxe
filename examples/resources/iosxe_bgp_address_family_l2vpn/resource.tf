2ad71b41b3e6e78dc9c2d45c5067b0c63b369dfd
Dan <78573620+Dan-Dev-Net@users.noreply.github.com>
2025-11-03 08:50:15 -0800

    Implement evpn advanced support (#324)

--- examples/resources/iosxe_bgp_address_family_l2vpn/resource.tf
+++ examples/resources/iosxe_bgp_address_family_l2vpn/resource.tf
@@ -1,4 +1,6 @@
 resource "iosxe_bgp_address_family_l2vpn" "example" {
-  asn     = "65000"
-  af_name = "evpn"
+  asn                       = "65000"
+  af_name                   = "evpn"
+  rewrite_evpn_rt_asn       = true
+  bgp_nexthop_trigger_delay = 10
 }
