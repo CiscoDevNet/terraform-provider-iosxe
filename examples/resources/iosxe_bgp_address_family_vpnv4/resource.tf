5bf21f1b485725e3bf6c49c0721be03eda292e52
Dan <78573620+Dan-Dev-Net@users.noreply.github.com>
2025-11-28 01:42:17 -0800

    Add bgp vpnv4 and vpnv6 af resources (#386)

---
+++ examples/resources/iosxe_bgp_address_family_vpnv4/resource.tf
@@ -0,0 +1,4 @@
+resource "iosxe_bgp_address_family_vpnv4" "example" {
+  asn     = "65000"
+  af_name = "unicast"
+}
---
+++ examples/resources/iosxe_bgp_address_family_vpnv6/import.sh
@@ -0,0 +1,1 @@
+terraform import iosxe_bgp_address_family_vpnv6.example "<asn>,<af_name>"
