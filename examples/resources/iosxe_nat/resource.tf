20d9db14968a96caa830e97602deba3edd406add
Andrea Testino <aitestino@gmail.com>
2026-03-13 15:16:41 -0400

    fix: apply terraform fmt to NAT example file

--- examples/resources/iosxe_nat/resource.tf
+++ examples/resources/iosxe_nat/resource.tf
@@ -2,24 +2,24 @@ resource "iosxe_nat" "example" {
   inside_source_interfaces = [
     {
       id = "10"
-        interfaces = [
-          {
-            interface = "GigabitEthernet4"
-            overload = true
-          }
-        ]
+      interfaces = [
+        {
+          interface = "GigabitEthernet4"
+          overload  = true
+        }
+      ]
     }
   ]
   inside_source_static_entries = [
     {
-      local_ip = "10.0.0.1"
+      local_ip  = "10.0.0.1"
       global_ip = "203.0.113.1"
     }
   ]
   outside_source_static_entries = [
     {
       global_ip = "198.51.100.1"
-      local_ip = "10.0.0.2"
+      local_ip  = "10.0.0.2"
     }
   ]
 }
